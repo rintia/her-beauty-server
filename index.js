556a9b06bab82cf537d4990b00f2f84513623ff2
Rintia Islam <rintiaislam110@gmail.com>
2023-10-20 15:43:26 +0600

    cart delete operation implemented

--- index.js
+++ index.js
@@ -94,6 +94,13 @@ async function run() {
       res.send(result);
   })
 
+  app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await cartsCollection.deleteOne(query);
+    res.send(result);
+})
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
